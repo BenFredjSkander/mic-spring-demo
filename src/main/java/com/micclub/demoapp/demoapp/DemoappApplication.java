61a9adcf902b6c0d9e1ffd67df4139257673e9c0
Skander <benfredjskander@hotmail.com>
2021-12-01 20:16:56 +0100

    first commit

---
+++ src/main/java/com/micclub/demoapp/demoapp/DemoappApplication.java
@@ -0,0 +1,13 @@
+package com.micclub.demoapp.demoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoappApplication.class, args);
+	}
+
+}
